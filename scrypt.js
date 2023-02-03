34c2c7eac5bbf096e1906267cea5de9a40b913b5
Aleksandra Mitiukova <106986719+AMitiukova@users.noreply.github.com>
2023-02-03 15:44:56 +0300

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,11 +1,11 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
       <script id="rendered-js" >
         
-$("#slider").on("input change", (e)=>{
+$("#slider").on("input change", e => {
   const sliderPos = e.target.value;
   // Update the width of the foreground image
-  $('.foreground-img').css('width', `${sliderPos}%`)
+  $('.foreground-img').css('width', `${sliderPos}%`);
   // Update the position of the slider button
-  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+  $('.slider-button').css('left', `calc(${sliderPos}% - 18px)`);
 });
  </script>
